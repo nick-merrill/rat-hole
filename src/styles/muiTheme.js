4bb89be8b03a2c968918bdd39ccd90ba2bd34d2f
Nick Merrill <git@nickmerrill.co>
2017-04-01 13:52:31 -0400

    Update structure with side menu instead of tabs

---
+++ src/styles/muiTheme.js
@@ -0,0 +1,10 @@
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {deepOrange500} from 'material-ui/styles/colors';
+
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: deepOrange500,
+  },
+});
+
+export default muiTheme;
