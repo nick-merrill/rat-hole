d038631738c1011b55cb11fcf0b2eb869ee2e506
Kunho <Kunho@Kunhos-MacBook-Air.local>
2017-04-15 15:58:58 -0400

    homepage!!

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,84 @@
 import React from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import Router from '../lib/Router';
+import {getCurrentUser, setCurrentUser} from '../data/users';
+import Avatar from 'material-ui/Avatar';
+import FileFolder from 'material-ui/svg-icons/file/folder';
+import FontIcon from 'material-ui/FontIcon';
+import List from 'material-ui/List/List';
+import ListItem from 'material-ui/List/ListItem';
+import Progress from '../vendors/progress/Progress';
+
+import {
+  blue300,
+  indigo900,
+  orange200,
+  deepOrange300,
+  pink400,
+  purple500,
+} from 'material-ui/styles/colors';
+
+
+
+const style = {
+  margin: 15,
+};
 
 class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      completed: 75,
+    };
+  }
+
+  handleClick(path) {
+    Router.goToPath(path);
+  }
+
   render() {
+    let currentUser = getCurrentUser();
     return (
-      <h1>Home Screen</h1>
+
+      <div>
+        <div>
+
+          <div style={{...style, paddingTop: 30, paddingBottom: 20}}>
+            Welcome, {currentUser.firstName}
+            <div>
+              <Avatar src={currentUser.imageURL}
+                      size={70}
+                      style={{margin: 5}}
+              />
+            </div>
+          </div>
+
+          <div style={{paddingBottom: 20}}>
+            <div style={{paddingBottom: 20}}>
+              Your Radar Percentage:
+            </div>
+            <Progress percent={90} />
+          </div>
+
+
+          <RaisedButton label="Test your knowledge"
+                        primary={true}
+                        style={style}
+                        onClick={() => this.handleClick('/game')}
+
+          />
+          <RaisedButton label="See my ranking"
+                        secondary={true}
+                        style={style}
+                        onClick={() => this.handleClick('/stats')}
+
+          />
+        </div>
+
+      </div>
+
+
+
     );
   }
 }
---
+++ src/vendors/progress/Progress.js
@@ -0,0 +1,39 @@
+
+var React    = require('react');
+var classSet = require('../../../../../Downloads/react-progress-form-master/src/js/utils/classSet');
+
+var ProgressElement = React.createClass({
+  componentDidUpdate: function() {
+    // var percent = parseInt(this.props.percent);
+  },
+  render: function() {
+    var percent = Math.floor(this.props.percent);
+    var deg = 360*percent/100;
+    var style = {
+      transform: 'rotate('+ deg +'deg)',
+    };
+    var classes = classSet({
+      "progress-pie-chart": true,
+      "gt-50": percent > 50
+    });
+    return (
+      <div className="progress clearfix">
+        <div className={classes}>
+          <div className="ppc-progress">
+            <div className="ppc-progress-fill"
+                 style={style}
+                 ref="progress">
+            </div>
+          </div>
+          <div className="ppc-percents">
+            <div className="pcc-percents-wrapper">
+              <span>{percent + '%'}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = ProgressElement;
