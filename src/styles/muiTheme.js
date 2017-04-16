d29d2c802d57b465b3a36af0ea75be245112384d
Nick Merrill <git@nickmerrill.co>
2017-04-16 11:22:04 -0400

    explain color scheme

--- src/styles/muiTheme.js
+++ src/styles/muiTheme.js
@@ -3,6 +3,13 @@ import {
   blue500, blueGrey500
 } from 'material-ui/styles/colors';
 
+// I don't think we want to go with Harvard branding because it makes an app
+// seem clunky and old. If we did go with school colors theme, it might make
+// it feel more like part of the school, but I'd be more inclined to do
+// house-specific branding, like a little bunny for Leverett on the home screen,
+// for example. For now though, I think going with a more modern color scheme
+// than Harvard's crimson will help us engage the user in the "game"
+// environment.
 const muiTheme = getMuiTheme({
   palette: {
     primary1Color: blue500,
