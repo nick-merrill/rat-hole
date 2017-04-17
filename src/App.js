885a77076bf42c8934f0459b048ee89099a77f5c
Nick Merrill <git@nickmerrill.co>
2017-04-17 10:23:16 -0400

    refactor app bar

--- src/App.js
+++ src/App.js
@@ -11,53 +11,19 @@ import './App.css';
 import _ from 'lodash';
 
 // MUI
-import AppBar from 'material-ui/AppBar';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import muiTheme from './styles/muiTheme';
 
 // Our Files
 import styles from './styles/styles';
-import {getCurrentUser, setCurrentUser} from './data/users';
+import {getCurrentUser} from './data/users';
 import Router from './lib/Router';
 import routes from './lib/routes';
 import Login from './screens/Login';
-import SideMenu from './components/SideMenu';
+import OurAppBar, {APP_BAR_HEIGHT} from './components/OurAppBar';
 
-/***************************************************
- * Define *additional* links for the side menu here.
- * For manual links, you must provide:
- * key
- * title
- * handleClick
- ****************************************************/
-let menuItems = [
-  {
-    route: _.find(routes, {key: 'studentList'}),
-  },
-  {
-    route: _.find(routes, {key: 'settings'}),
-  },
-  {
-    key: 'logout',
-    title: 'Sign Out',
-    labelPosition: 'left',
-    icon: <i className='fa fa-sign-out'/>,
-    color: muiTheme.palette.dangerColor,
-    handleClick: () => {
-      setCurrentUser(null);
-    },
-  },
-];
 
 class App extends Component {
-  constructor(props, context) {
-    super(props, context);
-
-    this.state = {
-      isMenuOpen: false,
-    };
-  }
-
   componentDidMount() {
     Router.subscribeToChange(() => {
       this.forceUpdate();
@@ -74,62 +40,6 @@ class App extends Component {
       You may need to go to http://localhost:3000/#/
       `);
     }
-
-    const appBarHeight = muiTheme.appBar.height;
-    const appBarStyle = {
-      position: 'fixed',
-      width: '100%',
-      left: 0,
-      right: 0,
-      top: 0,
-      margin: 0,
-      height: appBarHeight,
-    };
-    const appBarTitleStyle = {
-      position: 'absolute',
-      textAlign: 'center',
-      margin: 0,
-      left: 0,
-      right: 0,
-      top: 0,
-    };
-    const HomeAppBar = () => (
-      <div>
-        <AppBar title=""
-                iconClassNameLeft='fa fa-bars'
-                onLeftIconButtonTouchTap={() => this.setState({isMenuOpen: true})}
-                style={appBarStyle}
-                titleStyle={appBarTitleStyle}/>
-        <SideMenu
-          open={this.state.isMenuOpen}
-          items={menuItems}
-          // Allows the menu to style the current page for reference
-          currentItemKey={currentPath}
-          // Allows the open state to be changed on touch outside of
-          // menu events.
-          onRequestChange={(open) => this.setState({isMenuOpen: open})}
-          handleClose={(itemProps) => {
-            this.setState({
-              isMenuOpen: false,
-            });
-          }}/>
-      </div>
-    );
-    const ScreenAppBar = () => (
-      <AppBar title={currentRoute.title}
-              iconClassNameLeft='fa fa-chevron-left'
-              onLeftIconButtonTouchTap={() => Router.goToPath('/')}
-              style={appBarStyle}
-              titleStyle={appBarTitleStyle}/>
-    );
-    // Returns the appropriate App Bar
-    const MainAppBar = () => {
-      if (currentRoute.key === 'home') {
-        return <HomeAppBar/>;
-      } else {
-        return <ScreenAppBar/>;
-      }
-    };
 
     /**
      * Shows the login page if the user is not signed in.
@@ -148,8 +58,8 @@ class App extends Component {
     } else {
       Screen = () => (
         <div>
-          <MainAppBar />
-          <div style={{...styles.container, paddingTop: appBarHeight}}>
+          <OurAppBar currentRoute={currentRoute} />
+          <div style={{...styles.container, paddingTop: APP_BAR_HEIGHT}}>
             <currentRoute.component />
           </div>
         </div>
---
+++ src/components/OurAppBar.js
@@ -0,0 +1,109 @@
+/***************************************************
+ * Define *additional* links for the side menu here.
+ * For manual links, you must provide:
+ * key
+ * title
+ * handleClick
+ ****************************************************/
+import React from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+
+import muiTheme from '../styles/muiTheme';
+import SideMenu from '../components/SideMenu';
+import Router from '../lib/Router';
+import routes from '../lib/routes';
+import {setCurrentUser} from '../data/users';
+import {AppBar} from 'material-ui';
+
+export const APP_BAR_HEIGHT = muiTheme.appBar.height;
+
+const menuItems = [
+  {
+    route: _.find(routes, {key: 'studentList'}),
+  },
+  {
+    route: _.find(routes, {key: 'settings'}),
+  },
+  {
+    key: 'logout',
+    title: 'Sign Out',
+    labelPosition: 'left',
+    icon: <i className='fa fa-sign-out'/>,
+    color: muiTheme.palette.dangerColor,
+    handleClick: () => {
+      setCurrentUser(null);
+    },
+  },
+];
+
+class OurAppBar extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      isMenuOpen: false,
+    };
+  }
+
+  render() {
+    const appBarStyle = {
+      position: 'fixed',
+      width: '100%',
+      left: 0,
+      right: 0,
+      top: 0,
+      margin: 0,
+      height: APP_BAR_HEIGHT,
+    };
+    const appBarTitleStyle = {
+      position: 'absolute',
+      textAlign: 'center',
+      margin: 0,
+      left: 0,
+      right: 0,
+      top: 0,
+    };
+    const currentPath = this.props.currentRoute.path;
+    const HomeAppBar = () => (
+      <div>
+        <AppBar title=""
+                iconClassNameLeft='fa fa-bars'
+                onLeftIconButtonTouchTap={() => this.setState({isMenuOpen: true})}
+                style={appBarStyle}
+                titleStyle={appBarTitleStyle}/>
+        <SideMenu
+          open={this.state.isMenuOpen}
+          items={menuItems}
+          // Allows the menu to style the current page for reference
+          currentItemKey={currentPath}
+          // Allows the open state to be changed on touch outside of
+          // menu events.
+          onRequestChange={(open) => this.setState({isMenuOpen: open})}
+          handleClose={(itemProps) => {
+            this.setState({
+              isMenuOpen: false,
+            });
+          }}/>
+      </div>
+    );
+    const ScreenAppBar = () => (
+      <AppBar title={this.props.currentRoute.title}
+              iconClassNameLeft='fa fa-chevron-left'
+              onLeftIconButtonTouchTap={() => Router.goToPath('/')}
+              style={appBarStyle}
+              titleStyle={appBarTitleStyle}/>
+    );
+    // Returns the appropriate App Bar
+    if (this.props.currentRoute.key === 'home') {
+      return <HomeAppBar/>;
+    } else {
+      return <ScreenAppBar/>;
+    }
+  }
+}
+
+OurAppBar.propTypes = {
+  currentRoute: PropTypes.object.isRequired,
+};
+
+export default OurAppBar;
